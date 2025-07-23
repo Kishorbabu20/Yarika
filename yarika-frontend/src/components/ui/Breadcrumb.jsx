a763ebe510fe6828753ccf035e98c0f25c99a4ce
Kishor <kishor201201@gmail.com>
2025-07-23 17:02:51 +0530

    re-updated

--- yarika-frontend/src/components/ui/Breadcrumb.jsx
+++ yarika-frontend/src/components/ui/Breadcrumb.jsx
@@ -11,7 +11,7 @@ const Breadcrumb = React.forwardRef((props, ref) => (
 const BreadcrumbList = React.forwardRef((props, ref) => {
   const { className, ...rest } = props
   return (
-    <ol
+    <ul
       ref={ref}
       className={cn(
         "flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5",
